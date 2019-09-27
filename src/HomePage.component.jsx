2c45b4ee79fdf8b2859d9339bea1253e0077771c
RAF <raf@MacBook-Pro-RAF.local>
2019-09-27 13:48:11 +0200

    starter_commit

---
+++ src/HomePage.component.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import './HomePage.styles.scss'
+const HomePage=()=> (
+
+    <div className="homepage">
+
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">CD's</h1>
+                    <span className="subtitle">Shop now</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Vinyls</h1>
+                    <span className="subtitle">Shop now</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Instruments</h1>
+                    <span className="subtitle">Shop now</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Genres</h1>
+                    <span className="subtitle">Shop now</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Dj Tools</h1>
+                    <span className="subtitle">Shop now</span>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default HomePage
+
